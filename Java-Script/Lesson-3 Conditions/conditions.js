314b7783243285ef5580e2d21b1e2c7038524941
ahmetcng33 <ahmetcingi333@gmail.com>
2022-12-15 11:25:35 +0300

    basic-calculator-finish

--- Java-Script/Lesson-3 Conditions/conditions.js
+++ Java-Script/Lesson-3 Conditions/conditions.js
@@ -1,4 +1,69 @@
 // -------- 4 Operations Calculator------------ //
 
-let num1 = prompt("Enter your first number")
-let num2 = prompt("Enter your second number")
+let operate = prompt("Which math operation do you want to do? ex: +, *, /, -")
+let num1 = +prompt("Enter your first number")
+let num2 = +prompt("Enter your second number")
+
+if(operate === "+"){
+    result=num1 + num2
+    console.log(`Your result is ${result}`)
+}
+
+else if(operate === "-"){
+    result=num1-num2
+    console.log(`Your result is ${result}`)
+}
+
+else if(operate === "*"){
+    result=num1*num2
+    console.log(`Your result is ${result}`)
+}
+
+else if(operate === "/"){
+    result=num1/num2
+    console.log(`Your result is ${result}`);
+}
+else{
+    console.log("You entered the wrong math operate")
+}
+
+
+
+// ------------------ Second Solution Switch Case --------------------//
+
+let number1=+prompt("Enter your first number")
+let operator=prompt("Which math operation do you want to do? ex: +, *, /, -")
+let number2 = +prompt("Enter your second number")
+
+switch(operator){
+    case "+":
+        conclusion = number1 + number2
+        break;
+
+    case "-":
+        conclusion = number1 - number2
+        break;
+
+    case "*":
+        conclusion = number1 * number2
+        break;
+
+    case "/":
+        conclusion = number1 / number2
+        break;
+    default:
+        console.log("You entered wrong math operate");
+}
+
+console.log(`${number1} ${operator} ${number2} = ${conclusion}`)
+
+
+
+// -------------------- Weakly Program ------------------------ //
+
+let day= toUpperCase(prompt("Enter the day you want to see"))
+
+console.log(day);
+// switch(day){
+//     case monday,tuesday,wednesday,thursday
+// }

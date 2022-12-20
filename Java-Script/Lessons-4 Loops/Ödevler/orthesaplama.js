e4ee9920d12aecf1d5051df378ca7fffb59696a1
ahmetcng33 <ahmetcingi333@gmail.com>
2022-12-20 18:36:20 +0300

    ödevlerbitti

--- Java-Script/Lessons-4 Loops/Ödevler/orthesaplama.js
+++ Java-Script/Lessons-4 Loops/Ödevler/orthesaplama.js
@@ -1,13 +1,11 @@
 // ------------------ Console'dan girilen sayilarin ortalamasini bulan programi yaziniz. Program ilk olarak kullanicidan kac tane sayi gireceginin almali bu bilgi isiginda girilen sayilarin ortalamasini hesaplayarak yazdirmalidir.------------------//
 
 let adet = Number(prompt("How many do you use number"))
-let ortalama = 0;
+let toplam = 0;
 
-for( i=1 ; i<=adet ; i++) {
+for(let i=1 ; i<=adet ; i++) {
     number = Number(prompt(`Enter your ${i}th number`))
-    let toplam=0;
-    toplam=toplam+number
-    ortalama=toplam/adet
+    toplam+=number
 }
 
-console.log(`Sayilarin ortalamasi ${ortalama}`)
+console.log(`Sayilarin ortalamasi ${toplam/adet}`)
---
+++ Java-Script/Lessons-4 Loops/Ödevler/üshesaplama.js
@@ -0,0 +1,8 @@
+let taban = Number(prompt("Taban değeri giriniz"))
+let üs = Number(prompt("üs değerini giriniz"))
+let sonuc = 1;
+
+for(i=1 ; i<=üs ; i++){
+   sonuc*=taban
+}
+console.log(sonuc);
